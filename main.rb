d429627a6dc15b2ab32a25e548895486572db619
Sam Schirmer <samschirmer@gmail.com>
2018-08-16 16:04:22 -0500

    initial commit

---
+++ main.rb
@@ -0,0 +1,32 @@
+require 'intercom'
+require 'dotenv/load'
+
+class Contact
+	attr_accessor :external_id, :hb_user_id, :first_name, :last_name, :email, :custom_fields
+	def initialize(c)
+		name = c.name.split
+		self.first_name = name.first
+		self.last_name = name.drop(1).join(' ').strip
+		self.external_id = c.id
+		self.email = c.email
+		self.custom_fields = c.custom_attributes
+
+		self.hb_user_id = c.custom_attributes['HB User ID']
+	end
+end
+
+puts ENV['INTERCOME_KEY']
+intercom = Intercom::Client.new token: ENV['INTERCOM_KEY']
+
+hb_contacts = Array.new
+users = intercom.users.all.each do |u|
+	hb_contacts.push(Contact.new(u))
+end
+
+hb_contacts.each do |hb|
+	puts "\ncontact object #{hb.external_id}: #{hb.first_name} #{hb.last_name} - #{hb.email}\nCustomfields:"
+	puts hb.custom_fields.each { |k,v| puts "#{k}: #{v}" }
+end
+
+
+
